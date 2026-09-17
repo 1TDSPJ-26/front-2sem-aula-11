4f4b0cd182c5564b2f16c92e25b11be40597d17b
dudupizzoli <edupizzolijr@gmail.com>
2026-09-17 12:00:35 -0300

    feat : Conectando-se à API de usuários do github através de uma função assíncrona em um useState. Exibindo os dados dessa API através de uma lista na rota UsuariosGithub.

--- my-app/src/components/Menu/index.tsx
+++ my-app/src/components/Menu/index.tsx
@@ -6,6 +6,7 @@ export default function Menu(){
         <ul>
             <li><Link to="/Home">Home</Link></li>
             <li><Link to="/produtos">Produtos</Link></li>
+            <li><Link to="/users/git">Usuários Git</Link></li>
         </ul>
         </nav>
     )
